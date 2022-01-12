5a78d87be5135224c8089a15e503fe09092eb4c4
douglascarteribordignon <douglasbordignon8@gmail.com>
2022-01-12 18:33:10 -0300

    Primeiro Commit

---
+++ lib/input_customizado.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ImputCustomizado extends StatelessWidget {
+
+  final String hint;
+  final bool obscure;
+  final Icon icon;
+
+  const ImputCustomizado(
+      {
+        Key? key,
+        required this.hint,
+        this.obscure = false,
+        this.icon = const Icon(Icons.person)
+      }
+  ) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      child: TextField(
+        obscureText: obscure,
+        decoration: InputDecoration(
+          icon: icon,
+          border: InputBorder.none,
+          hintText: hint,
+          hintStyle: const TextStyle(
+              color: Colors.grey,
+              fontSize: 18
+          ),
+        ),
+      ),
+    );
+  }
+}
